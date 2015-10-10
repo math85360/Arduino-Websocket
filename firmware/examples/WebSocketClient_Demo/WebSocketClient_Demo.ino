2e487f8b2313379a6bd2601a3b617e2dffadd26c
Mathieu Leguey <mathieu.leguey@gmail.com>
2015-10-10 10:07:50 +0200

    Change examples to be compatible with particle.io

--- firmware/examples/WebSocketClient_Demo/WebSocketClient_Demo.ino
+++ firmware/examples/WebSocketClient_Demo/WebSocketClient_Demo.ino
@@ -1,6 +1,3 @@
-#include <SPI.h>
-#include <SC16IS750.h>
-#include <WiFly.h>
 
 // Here we define a maximum framelength to 64 bytes. Default is 256.
 #define MAX_FRAME_LENGTH 64
@@ -10,50 +7,15 @@
 
 #include <WebSocketClient.h>
 
-WiFlyClient client = WiFlyClient();
+TCPClient client = TCPClient();
 WebSocketClient webSocketClient;
 
 void setup() {
   
 
   Serial.begin(9600);
-  SC16IS750.begin();
   
-  WiFly.setUart(&SC16IS750);
   
-  WiFly.begin();
-  
-  // This is for an unsecured network
-  // For a WPA1/2 network use auth 3, and in another command send 'set wlan phrase PASSWORD'
-  // For a WEP network use auth 2, and in another command send 'set wlan key KEY'
-  WiFly.sendCommand(F("set wlan auth 1"));
-  WiFly.sendCommand(F("set wlan channel 0"));
-  WiFly.sendCommand(F("set ip dhcp 1"));
-  
-  Serial.println(F("Joining WiFi network..."));
-  
-
-  // Here is where you set the network name to join
-  if (!WiFly.sendCommand(F("join arduino_wifi"), "Associated!", 20000, false)) {    
-    Serial.println(F("Association failed."));
-    while (1) {
-      // Hang on failure.
-    }
-  }
-  
-  if (!WiFly.waitForResponse("DHCP in", 10000)) {  
-    Serial.println(F("DHCP failed."));
-    while (1) {
-      // Hang on failure.
-    }
-  }
-
-  // This is how you get the local IP as an IPAddress object
-  Serial.println(WiFly.localIP());
-  
-  // This delay is needed to let the WiFly respond properly
-  delay(100);
-
   // Connect to the websocket server
   if (client.connect("echo.websocket.org", 80)) {
     Serial.println("Connected");
--- firmware/examples/WebSocketServer_Demo/WebSocketServer_Demo.ino
+++ firmware/examples/WebSocketServer_Demo/WebSocketServer_Demo.ino
@@ -1,6 +1,3 @@
-#include <SPI.h>
-#include <SC16IS750.h>
-#include <WiFly.h>
 
 // Enabe debug tracing to Serial port.
 #define DEBUGGING
@@ -13,7 +10,7 @@
 
 #include <WebSocketServer.h>
 
-WiFlyServer server(80);
+TCPServer server(80);
 WebSocketServer webSocketServer;
 
 
@@ -60,48 +57,15 @@ void setup() {
   
 
   Serial.begin(9600);
-  SC16IS750.begin();
-  
-  WiFly.setUart(&SC16IS750);
-  
-  WiFly.begin();
-  
-  // This is for an unsecured network
-  // For a WPA1/2 network use auth 3, and in another command send 'set wlan phrase PASSWORD'
-  // For a WEP network use auth 2, and in another command send 'set wlan key KEY'
-  WiFly.sendCommand(F("set wlan auth 1"));
-  WiFly.sendCommand(F("set wlan channel 0"));
-  WiFly.sendCommand(F("set ip dhcp 1"));
   
   server.begin();
   Serial.println(F("Joining WiFi network..."));
   
-
-  // Here is where you set the network name to join
-  if (!WiFly.sendCommand(F("join arduino_wifi"), "Associated!", 20000, false)) {
-    Serial.println(F("Association failed."));
-    while (1) {
-      // Hang on failure.
-    }
-  }
-  
-  if (!WiFly.waitForResponse("DHCP in", 10000)) {  
-    Serial.println(F("DHCP failed."));
-    while (1) {
-      // Hang on failure.
-    }
-  }
-
-  // This is how you get the local IP as an IPAddress object
-  Serial.println(WiFly.localIP());
-  
-  // This delay is needed to let the WiFly respond properly
-  delay(100);
 }
 
 void loop() {
   String data;
-  WiFlyClient client = server.available();
+  TCPClient client = server.available();
   
   if (client.connected() && webSocketServer.handshake(client)) {
     
